009d104e6305b5f21cc6e399b5767fd742d40850
Olivia Hsieh <ohsieh@scu.edu>
2016-10-16 05:55:14 -0500

    Add files via upload

---
+++ client.js
@@ -0,0 +1,49 @@
+// var data = JSONData.slice();
+// var format = d3.json([url], [callbackfunction])
+// to take in JSON real time: http://pothibo.com/2013/09/d3-js-how-to-handle-dynamic-json-data/
+
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.productivity; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("./data.csv", type, function(error, data) {
+   if (error) throw error;
+
+  var g = svg.selectAll(".arc")
+    .data(pie(data))
+    .enter().append("g")
+    .attr("class", "arc");
+
+  g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data.activity); });
+
+  g.append("text")
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .text(function(d) { return d.data.activity; });
+});
+
+function type(d) {
+  d.productivity = +d.productivity;
+  return d;
+}
+
+// adding a second graph using d3: http://bl.ocks.org/d3noob/5987480
